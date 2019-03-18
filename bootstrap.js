903e46dc908896ab2f943974261d1471fc972e7c
Nolan Ehrstrom <github@nolanpro.com>
2019-03-18 15:09:56 -0700

    Update to include api

--- bootstrap.js
+++ bootstrap.js
@@ -1,56 +1,41 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fs = require("fs");
-// global['data'] = {}
-// global['config'] = {}
-// let data = {};
-// let config = {};
-function getFilePromise(file, callback) {
+var fs = require('fs');
+var script = require('./script_wrapped.js')
+var api = require('process_maker_api')
+
+function getFilePromise(file) {
     return new Promise((resolve, reject) => {
         fs.readFile(file, "utf8", (err, script) => {
             if (err) {
-                reject(err);
-                return;
+                reject(err)
+                return
             }
-            resolve(JSON.parse(script));
-        });
-    });
+            resolve(JSON.parse(script))
+        })
+    })
 }
-// function getScriptPromise(file: string) : Promise<string> {
-//     return new Promise((resolve, reject) => {
-//         exec('tsc -p tsconfig_script.json', (err) => {
-//             if (err) {
-//                 reject(err)
-//                 return
-//             }
-//             fs.readFile(file, "utf8", (err, script) => {
-//                 if (err) {
-//                     reject(err)
-//                     return
-//                 }
-//                 resolve(script)
-//             })
-//         })
-//     })
-// }
-// function evalScript(script: string) : void|object
-// {
-//     eval(script);
-// }
-const getData = getFilePromise('data.json');
-const getConfig = getFilePromise('config.json');
-// const getScript = getScriptPromise('script.js')
-const getScript = Promise.resolve('ok');
-const script_1 = require("./script");
-Promise.all([getScript, getConfig, getData]).then(function (values) {
-    // const script = values[0]
-    const config = values[1];
-    const data = values[2];
-    let output = {};
-    const result = script_1.default(data, config);
-    console.log("GOT RESULT ", result);
-    if (typeof result === 'object') {
-        output = Object.assign(data, result);
-    }
-    fs.writeFileSync('output.json', JSON.stringify(output));
-});
+
+const getData = getFilePromise('data.json')
+const getConfig = getFilePromise('config.json')
+
+Promise.all([getConfig, getData]).then(function(values) {
+    const config = values[0]
+    const data   = values[1]
+    let output = {}
+
+    let client = api.ApiClient.instance
+    // client.basePath = "http://bpm4.local.processmaker.com/api/1.0"
+    client.basePath = process.env.API_HOST
+
+    let auth = client.authentications['pm_api_bearer'];
+    auth.accessToken = process.env.API_TOKEN
+
+    const return_value = script.run(data, config, api);
+
+    Promise.resolve(return_value).then((result) => {
+        if (typeof result === 'object') {
+            output = Object.assign(data, result)
+        }
+        console.log("WRITING OUTPUT", output)
+        fs.writeFileSync('output.json', JSON.stringify(output))
+    });
+})
---
+++ run.sh
@@ -0,0 +1,6 @@
+SCRIPT=$(<script.js)
+SCRIPT_WRAPPED="exports.run = (data, config, api) => {
+    ${SCRIPT}
+}"
+echo "$SCRIPT_WRAPPED" > script_wrapped.js
+node bootstrap.js
