fdf5d6d0a447b56b7d75febca6f1a88a76bce04e
Nolan Ehrstrom <github@nolanpro.com>
2019-03-19 16:44:02 -0700

    Clone and copy SDK in so it will be available for docker

--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@ set -x
 
 BRANCH=${BRANCH:=master}
 TAG=${TAG:=dev-$BRANCH}
-REPO=${REPO:=ProcessMaker/pm4-sdk-node}
+
+if [[ ! -d "node-sdk" ]]; then
+    git clone --branch $BRANCH --depth 1 https://github.com/ProcessMaker/pm4-sdk-node.git node-sdk
+fi
 
 docker build -t processmaker/pm4-docker-executor-node:${TAG} .
+rm -rf node-sdk
 # Push to dockerhub here
