9182c0936fc1cb515ec608ca97376640ce00fa7b
Nolan Ehrstrom <github@nolanpro.com>
2019-03-19 17:17:57 -0700

    Replace slash with dash for docker tag

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -e
 set -x
 
 BRANCH=${BRANCH:=master}
-TAG=${TAG:=dev-$BRANCH}
+TAG=${TAG:=dev-${BRANCH//[\/]/-}}
 
 if [[ ! -d "node-sdk" ]]; then
     git clone --branch $BRANCH --depth 1 https://github.com/ProcessMaker/pm4-sdk-node.git node-sdk
