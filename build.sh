2abc79980a0f23bbea592986177ccdcd3742b5f5
Nolan Ehrstrom <github@nolanpro.com>
2019-04-08 12:35:37 -0700

    Update packge names to spark

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ BRANCH=${BRANCH:=master}
 TAG=${TAG:=dev-${BRANCH//[\/]/-}}
 
 pushd src
-    if [[ ! -d "pm4-sdk-node" ]]; then
-        git clone --branch $BRANCH --depth 1 https://github.com/ProcessMaker/pm4-sdk-node.git
+    if [[ ! -d "spark-sdk-node" ]]; then
+        git clone --branch $BRANCH --depth 1 https://github.com/ProcessMaker/spark-sdk-node.git
     fi
     rm -rf node_modules
     rm -rf package-lock.json
 popd
 
-docker build -t processmaker/pm4-docker-executor-node:${TAG} .
-rm -rf src/pm4-sdk-node
+docker build -t processmaker/spark-docker-executor-node:${TAG} .
+rm -rf src/spark-sdk-node
 # Push to dockerhub here
